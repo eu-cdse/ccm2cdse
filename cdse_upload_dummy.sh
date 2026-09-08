9e9b59b482fd438af7df66b39b0290d4fa46f87e
Jan Musial <jmusial@cloudferro.com>
2026-09-08 09:28:30 +0200

    Refactor variable exports and definitions in script
    Reordered export statements and defined variables for clarity.

--- cdse_upload_dummy.sh
+++ cdse_upload_dummy.sh
@@ -2,17 +2,17 @@
 #please replace PROVIDER_S3_PUBLIC_KEY, PROVIDER_S3_PRIVATE_KEY and CCM-<PROVIDER_NAME>-DEV with your credentials and your name 
 ############################### export variables
 export RCLONE_CONFIG_CLMS_TYPE=s3
-export RCLONE_CONFIG_CLMS_ACCESS_KEY_ID=<PROVIDER_S3_PUBLIC_KEY>
-export RCLONE_CONFIG_CLMS_SECRET_ACCESS_KEY=<PROVIDER_S3_PRIVATE_KEY>
 export RCLONE_CONFIG_CLMS_REGION=default
 export RCLONE_CONFIG_CLMS_ENDPOINT='https://s3.waw3-1.cloudferro.com'
 export RCLONE_CONFIG_CLMS_PROVIDER='Ceph'
 export PATH="${HOME}/Dropbox/scripts/bash:$PATH"
 ############################### define variables
-working_dir=/tmp
-dummy_product_name='dummy_product_name'
+export RCLONE_CONFIG_CLMS_ACCESS_KEY_ID=<PROVIDER_S3_PUBLIC_KEY>
+export RCLONE_CONFIG_CLMS_SECRET_ACCESS_KEY=<PROVIDER_S3_PRIVATE_KEY>
 bucket=CCM-<PROVIDER_NAME>-DEV
 ############################### create a dummy product with a random layer
+working_dir=/tmp
+dummy_product_name='dummy_product_name'
 mkdir -p $working_dir/${dummy_product_name}/${dummy_product_name}
 cd $working_dir/${dummy_product_name}
 gdal_create -outsize 20 20 -a_srs EPSG:4326 -a_ullr 2 50 3 49 -burn 10 ./${dummy_product_name}/dummy.tif
